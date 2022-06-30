31a4f0ac50de5cedf9c740d7d52b562169ef5175
Reynald Adolphe <100732140+theReynald@users.noreply.github.com>
2022-06-30 10:56:57 -0700

    Changed on Codespaces

--- sample.js
+++ sample.js
@@ -1,4 +1,4 @@
 console.log("-------------------");
-console.log("Rise & Shine!");
+console.log("Rise & Shine!!!!");
 console.log("Ready for a new day...."); 
 console.log("-------------------");     
